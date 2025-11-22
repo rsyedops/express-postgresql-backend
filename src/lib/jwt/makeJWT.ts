60647823491315687b8413bce50d65a459f006b1
Ahmed Fawzy <ahmedfawzy1799@gmail.com>
2025-11-22 23:50:14 +0200

    fix(lib): jwt exp

--- src/lib/jwt/makeJWT.ts
+++ src/lib/jwt/makeJWT.ts
@@ -1,11 +1,11 @@
 import { env } from "#config/env.js";
 import jwt from "jsonwebtoken";
 
-type Payload = Pick<jwt.JwtPayload, "exp" | "iat" | "iss" | "sub">;
+export type Payload = Pick<jwt.JwtPayload, "exp" | "iat" | "iss" | "sub">;
 
 export const makeJWT = (id: string) => {
   const payload: Payload = {
-    exp: env.JWT_EXP,
+    exp: Date.now() + env.JWT_EXP,
     iat: Date.now(),
     iss: env.JWT_ISSUER,
     sub: id,
